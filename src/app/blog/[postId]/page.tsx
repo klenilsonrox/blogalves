c61b4c18b0acb385dd2a1c0572941d8a0e465d5e
klenilsonrox <klenilsoncoc@gmail.com>
2023-11-22 02:03:37 -0300

    first commit

---
+++ src/app/blog/[postId]/page.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+
+interface PostProps {
+    params:{
+        postId:number
+    }
+    title:string,
+    body:string,
+    id:number,
+}
+
+const page =async ({params}:PostProps) => {
+
+    const reqPost= await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`)
+const postJson = await reqPost.json()
+
+
+  return (
+    <div className='flex items-center w-screen h-screen justify-center p-2'>
+    
+     <div className='p-2 shadow-lg w-screen max-w-md flex flex-col gap-2 relative'>
+     <button className='absolute pb-2 right-0 top-[-35px]'><a href="/blog" className='text-red-600'>← voltar</a></button>
+     <h1 className='text-[23px]  leading-6 font-semibold'>{postJson.title}</h1>
+     <p>{postJson.body}</p>
+     </div>
+    </div>
+  )
+}
+
+export default page
